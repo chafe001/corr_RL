c649bff49105440a49440b761ec61dad2aedfcb6
chafe001 <chafe001@umn.edu>
2024-09-09 17:09:52 -0500

    corr_RL v3 edits

---
+++ corr_RL task code/corr_RL_setCodes_v3.m
@@ -0,0 +1,24 @@
+function codes = corr_RL_setCodes_v3()
+
+% -------------------------------------------------------------------------
+% --------------------------- EVENT CODES ---------------------------------
+% -------------------------------------------------------------------------
+% Event codes are time stamps for trial events
+codes.startFix = 10;
+codes.preMovie = 20;
+codes.imgPair_on = 30;
+codes.imgPair_off = 40;
+codes.img1_on = 50;
+codes.img1_off = 60;
+codes.img2_on = 70;
+codes.img2_off = 80;
+codes.endMovie = 90;
+codes.beginRespWindow = 100;
+codes.response_key1 = 110;
+codes.response_key2 = 120;
+codes.noResponse = 130;
+codes.choiceRing_on = 140;
+codes.errorRing_on = 150;
+codes.rewRing_on = 160;
+
+end
