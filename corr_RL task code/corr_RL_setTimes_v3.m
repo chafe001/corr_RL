4c873fb6ecdd810f0e2a63c5f561830326a597a4
Sam Brunson <56322279+brunsons98@users.noreply.github.com>
2024-09-11 10:00:14 -0500

    corr_RL v3 updates curves

--- corr_RL task code/corr_RL_setTimes_v3.m
+++ corr_RL task code/corr_RL_setTimes_v3.m
@@ -148,6 +148,32 @@ case 'LAPTOP-42DLT8TH'
         times.rewRing_frames = 10;
         times.scError_ms = 1000;
 
+    case 'DESKTOP-RJQAES2'
+
+        % laptop at 120 Hz
+        times.stim_frames = 1;
+        times.soa_frames = 1;
+        times.interPair_frames = 5;
+
+        % times for curve stimuli
+        times.curve_frames = 3;
+
+        % times below specified in screen refresh units, absolute time depends on
+        % graphics refresh rate, 60 Hz in desktop workstations (typically), 120 Hz
+        % in new laptops, etc
+        % scene 1 is pretrial
+        % times.fixDur_ms = 20;  % fix targ only
+        times.preMovie_frames = 20;  % fix targ and choices
+        times.postMovie_frames = 20;
+        times.sc1_pretrial_ms = 20;
+        times.sc3_response_ms = 2500;
+        times.sc4_feedback_frames = 20; % duration of scene depends on choice ring timing
+        times.choiceRing_frames = 10;
+        times.rewRing_frames = 10;
+        times.scError_ms = 1000;
+
+
+
 end
 
 % -------------------------------------------------------------------------
