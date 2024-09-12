9ab111f7d30bd04fd868527a8e07927871f6e626
chafe001 <chafe001@umn.edu>
2024-09-12 18:10:37 -0500

    genCurve_v2 updates

--- corr_RL task code/genCurves_v2.m
+++ corr_RL task code/genCurves_v2.m
@@ -70,8 +70,11 @@ function [] = genCurves_v2()
 
 params.nBlocks = 10;
 params.nStates = 2;
-params.nMoviesPerState = 50;
-params.nCurvesPerMovie = 10;
+params.n_tvals_ = 50;
+
+
+params.n_tvals_main = 20;
+params.n_tvals_ortho = 7;
 
 params.size_of_knot_grid = 10;
 params.low = -1;
@@ -81,7 +84,8 @@ params.max_knot_number = 10;
 params.D = 256; % resolution_of_curves
 params.K = 2; % number of axes in curve space that we will sample
 % params.S = 1; % number of "exposures" to generate - not used presently
-params.N = 50; % number of samples per exposure
+params.N_curves_main = 50; % number of endcurves per state
+params.N_curves_ortho = 6; % number of orthocurves per state
 params.save_png = true;
 params.plotCurveSeq = true;
 params.n_knot_points = 5;
@@ -103,16 +107,23 @@ for b = 1:params.nBlocks
     % --- BLOCK LEVEL operations
     % compute endcurve (signal) and orthocurve (noise or reward) for this block
 
-    % endcurve
+    % define random endcurve
     knots = sample_knots(knot_grid, params.n_knot_points, false);
     [endcurve, ~] = upsample(knots, params.D);
 
-    % orthocurve
+    % define random orthocurve
     el = sum(sum(endcurve .* endcurve));
     knots = sample_knots(knot_grid, params.n_knot_points, false);
     [other_curve, ~] = upsample(knots, params.D);
     orthocurve = sum(sum(other_curve .* endcurve)) * endcurve / el - other_curve;
-    fprintf('check: %.2f\n', sum(sum(orthocurve .* endcurve))); % ??
+    fprintf('check: %.2f\n', sum(sum(orthocurve .* endcurve)));
+
+    % plot endcurve and orthocurve
+    figure;
+    plot(endcurve(:, 1), endcurve(:, 2));
+    hold on;
+    plot(orthocurve(:, 1), orthocurve(:, 2), 'r');
+
 
     for s = 1:params.nStates  % 2 states per block (L/R response)
 
@@ -125,120 +136,152 @@ for b = 1:params.nBlocks
         % and second halves of manifold for now
         switch s
             case 1
-                lower_t_lim = -1;
-                upper_t_lim = 0;
+                main_lowerT = -1;
+                main_upperT = 0;
+                ortho_lowerT = -1;
+                ortho_upperT = 1;
             case 2
-                lower_t_lim = 0;
-                upper_t_lim = 1;
+                main_lowerT = 0;
+                main_upperT = 1;
+                ortho_lowerT = -1;
+                ortho_upperT = 1;
         end
 
-        % establish vector of t values corresponding to individual curves
-        % in range for this state
-        t = interp1([1, params.N], [lower_t_lim, upper_t_lim], 1:params.N);
+        % --- set range of t values specifying weights with which endcurve
+        % and orthocurve are summed to produce individual curves
+        endcurve_t = interp1([1, params.n_tvals_main], [main_lowerT, main_upperT], 1:params.n_tvals_main);
+        orthocurve_t = interp1([1, params.n_tvals_ortho], [ortho_lowerT, ortho_upperT], 1:params.n_tvals_ortho);
 
+        % build a 2D matrix of curves defined by endcurve and orthocurve t
+        % values
+        for ec = 1 : length(endcurve_t)
+            for oc = 1 : length(orthocurve_t)
+                state_curves(ec, oc, :, :) = curve_of_origin + (endcurve_t(ec) * endcurve) + (orthocurve_t(oc) * orthocurve);
+            end
+        end
 
-        for m = 1:params.nMoviesPerState % number distinct movies, sections of manifold, per state
+        if params.plotCurveSeq
+            lim = 1.5;
+            limx = [-lim, lim];
+            limy = [-lim, lim];
 
+            figure;
 
+            for ec = 1: length(endcurve_t)
+                for oc = 1: length(orthocurve_t)
+                    subplot(length(orthocurve_t), length(endcurve_t), (ec-1)*length(orthocurve_t)+oc);
+                    x = state_curves(ec,oc,:,1);
+                    x = squeeze(x);
+                    y = state_curves(ec,oc,:,2);
+                    y = squeeze(y);
+                    plot(x,y);
+                    xlim(limx);
+                    ylim(limy);
+                    axis off;
+                    axis equal;
 
-            for c = 1:params.nCurvesPerMovie
-
-
-                % Travel smoothly along manifold, sample a curve at each point
-                smooth_sequence = zeros(params.N, params.D, 2);
-                for i = 1:params.N
-                    t = interp1([1, params.N], [-scale_lim, scale_lim], i);
-                    smooth_sequence(i, :, :) = curve_of_origin + t * endcurve;
                 end
+            end
 
-                % Random samples of curves along an orthogonal manifold: this is the "noise"
-                rough_sequence = zeros(params.N, params.D, 2);
-                for n = 1:params.N
-                    t = max(min(randn, scale_lim), -scale_lim);
-                    rough_sequence(n, :, :) = curve_of_origin + t * orthocurve;
-                end
+        end
 
 
-
-                %%
-                % Plotting
-                figure;
-                plot(endcurve(:, 1), endcurve(:, 2));
-                hold on;
-                plot(orthocurve(:, 1), orthocurve(:, 2), 'r');
-
-                %%
-
-                if params.plotCurveSeq
-                    % lim = 1.75;
-                    lim = 1.5;
-                    limx = [-lim, lim];
-                    limy = [-lim, lim];
-
+        if params.save_png
+            for ec = 1: length(endcurve_t)
+                for oc = 1: length(orthocurve_t)
                     figure;
-                    for i = 1:3
-                        for n = 1:params.N
-                            subplot(3, params.N, (i-1)*params.N + n);
-                            if i == 1
-                                plot(smooth_sequence(n, :, 1), smooth_sequence(n, :, 2));
-                            elseif i == 2
-                                plot(rough_sequence(n, :, 1), rough_sequence(n, :, 2));
-                            else
-                                plot((rough_sequence(n, :, 1) + smooth_sequence(n, :, 1))/2, ...
-                                    (rough_sequence(n, :, 2) + smooth_sequence(n, :, 2))/2);
-                            end
-                            xlim(limx);
-                            ylim(limy);
-                            axis off;
-                            axis equal;
-                        end
-                    end
+                    x = state_curves(ec,oc,:,1);
+                    x = squeeze(x);
+                    y = state_curves(ec,oc,:,2);
+                    y = squeeze(y);
+                    plot(x,y);
+                    xlim(limx);
+                    ylim(limy);
+                    axis off;
+                    axis equal;
+
+                    --- SAVE PNG
+                    state = 'A';
+                    fn = buildFilename(params, i, n, b, c);
+                    cd blockstim
+                    % f is figure object, if not included in print command, prints
+                    % last ML screen changed, eg user, as png file
+                    print(f, fn, '-dpng');
+                    close;
+                    cd ..
 
                 end
+            end
 
-                %%
-                if params.save_png
-
-                    for i = 1:3
-                        for n = 1:params.N
-                            % subplot(3, params.N, (i-1)*params.N + n);
-                            if i == 1
-                                f = figure;
-                                plot(smooth_sequence(n, :, 1), smooth_sequence(n, :, 2));
-                            elseif i == 2
-                                f = figure;
-                                plot(rough_sequence(n, :, 1), rough_sequence(n, :, 2));
-                            else
-                                f = figure;
-                                plot((rough_sequence(n, :, 1) + smooth_sequence(n, :, 1))/2, ...
-                                    (rough_sequence(n, :, 2) + smooth_sequence(n, :, 2))/2);
-                            end
-                            xlim(limx);
-                            ylim(limy);
-                            axis off;
-                            axis equal;
-
-                            % --- SAVE PNG
-                            state = 'A';
-                            fn = buildFilename(params, i, n, b, c);
-                            cd blockstim
-                            % f is figure object, if not included in print command, prints
-                            % last ML screen changed, eg user, as png file
-                            print(f, fn, '-dpng');
-                            close;
-                            cd ..
-
-                        end
-                    end
-
-                end % save_png
+        end
 
 
 
-            end % nCurvesPerMovie
-        end % nMoviesPerState
-    end % nStates
-end  % nBlocks
+
+
+
+    end
+
+
+
+    bob = 1;
+
+
+
+
+
+
+
+
+
+
+end
+
+
+end
+
+
+
+%
+% %%
+% if params.save_png
+%
+%     for i = 1:3
+%         for n = 1:params.N
+%             % subplot(3, params.N, (i-1)*params.N + n);
+%             if i == 1
+%                 f = figure;
+%                 plot(smooth_sequence(n, :, 1), smooth_sequence(n, :, 2));
+%             elseif i == 2
+%                 f = figure;
+%                 plot(rough_sequence(n, :, 1), rough_sequence(n, :, 2));
+%             else
+%                 f = figure;
+%                 plot((rough_sequence(n, :, 1) + smooth_sequence(n, :, 1))/2, ...
+%                     (rough_sequence(n, :, 2) + smooth_sequence(n, :, 2))/2);
+%             end
+%             xlim(limx);
+%             ylim(limy);
+%             axis off;
+%             axis equal;
+%
+%             % --- SAVE PNG
+%             state = 'A';
+%             fn = buildFilename(params, i, n, b, c);
+%             cd blockstim
+%             % f is figure object, if not included in print command, prints
+%             % last ML screen changed, eg user, as png file
+%             print(f, fn, '-dpng');
+%             close;
+%             cd ..
+%
+%         end
+%     end
+%
+% end % save_png
+
+
+
 
 
 
@@ -247,32 +290,32 @@ end  % nBlocks
 %%
 % Utilities for generating curves
 
-    function fn = buildFilename(params, i, n, b, c)
+function fn = buildFilename(params, i, n, b, c)
 
-        blockstr = strcat ('b', num2str(b), '_');
-        curvestr = strcat ('c', num2str(c), '_');
+blockstr = strcat ('b', num2str(b), '_');
+curvestr = strcat ('c', num2str(c), '_');
 
-        switch i
+switch i
 
-            case 1
-                typeStr = 'smooth_';
+    case 1
+        typeStr = 'smooth_';
 
-            case 2
-                typeStr = 'rough_';
+    case 2
+        typeStr = 'rough_';
 
-            case 3
-                typeStr = 'comb_';
+    case 3
+        typeStr = 'comb_';
 
-        end
+end
 
-        sampStr = strcat('s', num2str(n));
+sampStr = strcat('s', num2str(n));
 
-        fn = strcat(typeStr, blockstr, curvestr, sampStr);
+fn = strcat(typeStr, blockstr, curvestr, sampStr);
 
-        bob = 1;
+bob = 1;
 
 
-    end
+end
 
 
 % function stacked = xy2stacked(xy)
@@ -280,138 +323,139 @@ end  % nBlocks
 %     stacked = reshape(xy', [n*2, 1]);
 % end
 
-    function xy = stacked2xy(stacked)
-        assert(mod(length(stacked), 2) == 0);
-        n = length(stacked) / 2;
-        xy = reshape(stacked, [2, n])';
-    end
+function xy = stacked2xy(stacked)
+assert(mod(length(stacked), 2) == 0);
+n = length(stacked) / 2;
+xy = reshape(stacked, [2, n])';
+end
 
-    function segmented = stacked2segmented(curve)
-        n_points = length(curve) / 2;
-        assert(mod(n_points, 1) == 0);
-        x1_idx = 1:n_points-1;
-        x2_idx = 2:n_points;
-        y1_idx = x1_idx + n_points;
-        y2_idx = x2_idx + n_points;
-        segmented = [curve(x1_idx), curve(x2_idx), curve(y1_idx), curve(y2_idx)];
-    end
+function segmented = stacked2segmented(curve)
+n_points = length(curve) / 2;
+assert(mod(n_points, 1) == 0);
+x1_idx = 1:n_points-1;
+x2_idx = 2:n_points;
+y1_idx = x1_idx + n_points;
+y2_idx = x2_idx + n_points;
+segmented = [curve(x1_idx), curve(x2_idx), curve(y1_idx), curve(y2_idx)];
+end
 
-    function probes_nxy = linear_probe_set(lim, num, center, dash)
-        if nargin < 3
-            center = [0, 0];
-        end
-        if nargin < 4
-            dash = false;
-        end
+function probes_nxy = linear_probe_set(lim, num, center, dash)
+if nargin < 3
+    center = [0, 0];
+end
+if nargin < 4
+    dash = false;
+end
 
-        cnt = 1;
-        base_probes = 6 * num;
-        if dash
-            num_probes = base_probes * (1 + dash);
-        else
-            num_probes = base_probes;
-        end
-        probes_nxy = zeros(num_probes, 2, 2);
-        fprintf('making %d probes\n', num_probes);
-        eps = 0.01; % avoid corners
+cnt = 1;
+base_probes = 6 * num;
+if dash
+    num_probes = base_probes * (1 + dash);
+else
+    num_probes = base_probes;
+end
+probes_nxy = zeros(num_probes, 2, 2);
+fprintf('making %d probes\n', num_probes);
+eps = 0.01; % avoid corners
 
-        % Vertical probes
-        for x = linspace(-lim+eps, lim-eps, num)
-            probes_nxy(cnt, :, :) = [x, lim; x, -lim];
+% Vertical probes
+for x = linspace(-lim+eps, lim-eps, num)
+    probes_nxy(cnt, :, :) = [x, lim; x, -lim];
+    cnt = cnt + 1;
+end
+
+% Horizontal probes
+for y = linspace(-lim+eps, lim-eps, num)
+    probes_nxy(cnt, :, :) = [lim, y; -lim, y];
+    cnt = cnt + 1;
+end
+
+% Negative oblique
+for y = linspace(-lim+eps, lim-eps, num)
+    probes_nxy(cnt, :, :) = [-lim, y; y, -lim];
+    cnt = cnt + 1;
+end
+for x = linspace(-lim+eps, lim-eps, num)
+    probes_nxy(cnt, :, :) = [x, lim; lim, x];
+    cnt = cnt + 1;
+end
+
+% Positive oblique
+for y = linspace(-lim+eps, lim-eps, num)
+    probes_nxy(cnt, :, :) = [lim, y; -y, -lim];
+    cnt = cnt + 1;
+end
+for x = linspace(-lim+eps, lim-eps, num)
+    probes_nxy(cnt, :, :) = [x, lim; -lim, -x];
+    cnt = cnt + 1;
+end
+
+if dash
+    inc = 1 / dash;
+    for i = 1:base_probes
+        xp = polyfit([0, 1], probes_nxy(i, :, 1)', 1);
+        yp = polyfit([0, 1], probes_nxy(i, :, 2)', 1);
+        stop = 0;
+        for j = 1:dash
+            start = stop;
+            stop = start + inc;
+            probes_nxy(cnt, :, 1) = polyval(xp, [start, stop]);
+            probes_nxy(cnt, :, 2) = polyval(yp, [start, stop]);
             cnt = cnt + 1;
         end
-
-        % Horizontal probes
-        for y = linspace(-lim+eps, lim-eps, num)
-            probes_nxy(cnt, :, :) = [lim, y; -lim, y];
-            cnt = cnt + 1;
-        end
-
-        % Negative oblique
-        for y = linspace(-lim+eps, lim-eps, num)
-            probes_nxy(cnt, :, :) = [-lim, y; y, -lim];
-            cnt = cnt + 1;
-        end
-        for x = linspace(-lim+eps, lim-eps, num)
-            probes_nxy(cnt, :, :) = [x, lim; lim, x];
-            cnt = cnt + 1;
-        end
-
-        % Positive oblique
-        for y = linspace(-lim+eps, lim-eps, num)
-            probes_nxy(cnt, :, :) = [lim, y; -y, -lim];
-            cnt = cnt + 1;
-        end
-        for x = linspace(-lim+eps, lim-eps, num)
-            probes_nxy(cnt, :, :) = [x, lim; -lim, -x];
-            cnt = cnt + 1;
-        end
-
-        if dash
-            inc = 1 / dash;
-            for i = 1:base_probes
-                xp = polyfit([0, 1], probes_nxy(i, :, 1)', 1);
-                yp = polyfit([0, 1], probes_nxy(i, :, 2)', 1);
-                stop = 0;
-                for j = 1:dash
-                    start = stop;
-                    stop = start + inc;
-                    probes_nxy(cnt, :, 1) = polyval(xp, [start, stop]);
-                    probes_nxy(cnt, :, 2) = polyval(yp, [start, stop]);
-                    cnt = cnt + 1;
-                end
-            end
-        end
-
-        probes_nxy = probes_nxy + repmat(reshape(center, [1, 1, 2]), [size(probes_nxy, 1), 2, 1]);
     end
+end
 
-    function grid = make_knot_grid(size, low, high)
-        [X, Y] = meshgrid(linspace(low, high, size));
-        grid = [X(:), Y(:)];
-    end
+probes_nxy = probes_nxy + repmat(reshape(center, [1, 1, 2]), [size(probes_nxy, 1), 2, 1]);
+end
 
-    function knots = sample_knots(knot_grid, number_of_knot_points, closed)
-        if nargin < 3
-            closed = true;
-        end
+function grid = make_knot_grid(size, low, high)
+[X, Y] = meshgrid(linspace(low, high, size));
+grid = [X(:), Y(:)];
+end
 
-        grid_size = size(knot_grid, 1);
-        if nargin < 2 || isempty(number_of_knot_points)
-            nkp = grid_size;
-        else
-            nkp = number_of_knot_points;
-        end
+function knots = sample_knots(knot_grid, number_of_knot_points, closed)
+if nargin < 3
+    closed = true;
+end
 
-        dice = randsample(grid_size, nkp, nkp > grid_size);
-        if closed
-            dice(end) = dice(1);
-        end
-        knots = knot_grid(dice, :);
-    end
+grid_size = size(knot_grid, 1);
+if nargin < 2 || isempty(number_of_knot_points)
+    nkp = grid_size;
+else
+    nkp = number_of_knot_points;
+end
 
-    function [xy, knots] = upsample(knots, resolution, kind)
-        if nargin < 3
-            kind = 'spline';
-        end
+dice = randsample(grid_size, nkp, nkp > grid_size);
+if closed
+    dice(end) = dice(1);
+end
+knots = knot_grid(dice, :);
+end
 
-        num_knots = size(knots, 1);
+function [xy, knots] = upsample(knots, resolution, kind)
+if nargin < 3
+    kind = 'spline';
+end
 
-        if num_knots < 3
-            kind = 'linear';
-        end
+num_knots = size(knots, 1);
 
-        plot_range = linspace(0, num_knots-1, resolution);
+if num_knots < 3
+    kind = 'linear';
+end
 
-        if strcmp(kind, 'linear')
-            fx = interp1(0:num_knots-1, knots(:,1), plot_range, 'linear');
-            fy = interp1(0:num_knots-1, knots(:,2), plot_range, 'linear');
-        else
-            fx = interp1(0:num_knots-1, knots(:,1), plot_range, 'spline');
-            fy = interp1(0:num_knots-1, knots(:,2), plot_range, 'spline');
-        end
+plot_range = linspace(0, num_knots-1, resolution);
 
-        xy = [fx', fy'];
-    end
+if strcmp(kind, 'linear')
+    fx = interp1(0:num_knots-1, knots(:,1), plot_range, 'linear');
+    fy = interp1(0:num_knots-1, knots(:,2), plot_range, 'linear');
+else
+    fx = interp1(0:num_knots-1, knots(:,1), plot_range, 'spline');
+    fy = interp1(0:num_knots-1, knots(:,2), plot_range, 'spline');
+end
+
+xy = [fx', fy'];
+end
+
 
 end
